88acea1ac3aea7cae05e8970ec065d0f04568833
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-08-30 15:09:55 -0300

    feat: R1 BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/album" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/search" component={ Search } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </div>
+    );
   }
 }
 
